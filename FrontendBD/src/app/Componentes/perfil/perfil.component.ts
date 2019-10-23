003f6985f100472c8246d17fa4123c1ef5cf9453
kelinn <36924700+kelinn@users.noreply.github.com>
2019-10-23 17:22:35 -0600

    perfil plantilla

--- FrontendBD/src/app/Componentes/perfil/perfil.component.ts
+++ FrontendBD/src/app/Componentes/perfil/perfil.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  public dataUser = {
+    nombre: null,
+    nombreUsuario: null,
+    direccion: null,
+    telefono: null,
+ 
+  };
 
+  public error: String;
+  public success: String;
+  public status: String;
   constructor() { }
 
   ngOnInit() {
   }
+  data(data) {
+    this.dataUser.nombre = data.nombre;
+    this.dataUser.nombreUsuario = data.nombreUsuario;
+    this.dataUser.direccion = data.direccion;
+    this.dataUser.telefono = data.telefono;
+  }
 
+  responseSuccess(data) {
+    this.success = data.data;
+    this.status = "success";
+  }
+  responseError(error) {
+    this.error = error.error.error;
+    this.status = "error";
+  }
+ 
 }
--- FrontendBD/src/app/app-routing.module.ts
+++ FrontendBD/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './Componentes/login/login.component';
 import { RegistrarComponent } from './Componentes/registrar/registrar.component';
-
+import { PerfilComponent } from './Componentes/perfil/perfil.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'registrar', component: RegistrarComponent},
+  { path: 'perfil', component: PerfilComponent},
 ];
 
 @NgModule({

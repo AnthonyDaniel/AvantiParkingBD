24b651bc0351799d3e1051e12b158b6361eea777
kelinn <36924700+kelinn@users.noreply.github.com>
2019-10-23 19:58:47 -0600

    Update vehiculo.service.ts

--- FrontendBD/src/app/Servicios/vehiculo.service.ts
+++ FrontendBD/src/app/Servicios/vehiculo.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoService {
 
-  private baseUrl = 'http://localhost:8000/api/vehiculo';
+  private baseUrl = 'http://127.0.0.1:8000/api/vehiculo';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listarVehiculo(){
+    this.http.get(`${this.baseUrl}`);
+  }
+  public guardarVehiculo(data){
+    this.http.post(`${this.baseUrl}/vehiculo`, data);
+  }
 
 }

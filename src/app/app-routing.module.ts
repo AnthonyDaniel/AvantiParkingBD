e0a2b88bbe3afc2642291cd3e40cf78af76e989f
kelinn <36924700+kelinn@users.noreply.github.com>
2019-10-25 21:43:18 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './Componentes/login/login.component';
 import { RegistrarComponent } from './Componentes/registrar/registrar.component';
 import { PerfilComponent } from './Componentes/perfil/perfil.component';
-import { VehiculoComponent } from './Componentes/vehiculo/vehiculo.component';
-import { ReservaComponent } from './Componentes/reserva/reserva.component';
 import { BeforeLoginService } from './Servicios/before-login.service';
 import { AfterLoginService } from './Servicios/after-login.service';
 import { AdministradorComponent } from './Componentes/administrador/administrador.component';
 import { AdminLoginService } from './Servicios/admin-login.service';
+import { VehiculoComponent } from './Componentes/vehiculo/vehiculo.component';
+import { ReservaComponent } from './Componentes/reserva/reserva.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [BeforeLoginService]},
   { path: 'registrar', component: RegistrarComponent,canActivate: [BeforeLoginService]},
-  { path: 'vehiculo', component: VehiculoComponent ,canActivate: [BeforeLoginService]},
-  { path: 'reserva', component: ReservaComponent, canActivate: [BeforeLoginService]},
+  { path: 'vehiculo', component: VehiculoComponent ,canActivate: [AfterLoginService]},
+  { path: 'reserva', component: ReservaComponent, canActivate: [AfterLoginService]},
   { path: 'perfil', component: PerfilComponent ,canActivate: [AfterLoginService]},
   { path: 'admin', component: AdministradorComponent, canActivate: [AdminLoginService]}
 
